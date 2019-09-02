b4fdfc1606ec4ecd63aa18a9d228ca558e181113
somayehkhanbabaei <somayeh.khanbabaei21@gmail.com>
2019-09-02 22:49:31 +0430

    make big component to small component

---
+++ src/components/books-section/books-container.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+const BooksContainer = ()=> {
+    return(
+        <div className="books-container">
+        <div className="book">
+          <div className="book-img">
+            <img src="https://via.placeholder.com/250" alt="" />
+          </div>
+          <div className="book-info">
+            <div className="book-title">
+              <h3> Book title </h3>
+            </div>
+            <div className="book-author">
+              <p> By Author</p>
+            </div>
+            <div className="">
+              <button className="btn btn-highlight"> Detail</button>
+            </div>
+          </div>
+        </div>
+        <div className="book">
+          <div className="book-img">
+            <img src="https://via.placeholder.com/250" alt="" />
+          </div>
+          <div className="book-info">
+            <div className="book-title">
+              <h3> Book title </h3>
+            </div>
+            <div className="book-author">
+              <p> By Author</p>
+            </div>
+            <div className="">
+              <button className="btn btn-highlight"> Detail</button>
+            </div>
+          </div>
+        </div>
+        <div className="book">
+          <div className="book-img">
+            <img src="https://via.placeholder.com/250" alt="" />
+          </div>
+          <div className="book-info">
+            <div className="book-title">
+              <h3> Book title </h3>
+            </div>
+            <div className="book-author">
+              <p> By Author</p>
+            </div>
+            <div className="">
+              <button className="btn btn-highlight"> Detail</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="book">
+          <div className="book-img">
+            <img src="https://via.placeholder.com/250" alt="" />
+          </div>
+          <div className="book-info">
+            <div className="book-title">
+              <h3> Book title </h3>
+            </div>
+            <div className="book-author">
+              <p> By Author</p>
+            </div>
+            <div className="">
+              <button className="btn btn-highlight"> Detail</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+
+}
+
+
+export default BooksContainer;
--- src/components/books-section/books-section.js
+++ src/components/books-section/books-section.js
@@ -1,4 +1,5 @@
 import React from "react";
+import BooksContainer from "./books-container";
 
 const BooksSection = props => {
   return (
@@ -8,73 +9,7 @@ const BooksSection = props => {
           <div className="section-title">
             <h1> HTML Books </h1>
           </div>
-          <div className="books-container">
-            <div className="book">
-              <div className="book-img">
-                <img src="https://via.placeholder.com/250" alt="" />
-              </div>
-              <div className="book-info">
-                <div className="book-title">
-                  <h3> Book title </h3>
-                </div>
-                <div className="book-author">
-                  <p> By Author</p>
-                </div>
-                <div className="">
-                  <button className="btn btn-highlight"> Detail</button>
-                </div>
-              </div>
-            </div>
-            <div className="book">
-              <div className="book-img">
-                <img src="https://via.placeholder.com/250" alt="" />
-              </div>
-              <div className="book-info">
-                <div className="book-title">
-                  <h3> Book title </h3>
-                </div>
-                <div className="book-author">
-                  <p> By Author</p>
-                </div>
-                <div className="">
-                  <button className="btn btn-highlight"> Detail</button>
-                </div>
-              </div>
-            </div>
-            <div className="book">
-              <div className="book-img">
-                <img src="https://via.placeholder.com/250" alt="" />
-              </div>
-              <div className="book-info">
-                <div className="book-title">
-                  <h3> Book title </h3>
-                </div>
-                <div className="book-author">
-                  <p> By Author</p>
-                </div>
-                <div className="">
-                  <button className="btn btn-highlight"> Detail</button>
-                </div>
-              </div>
-            </div>
-
-            <div className="book">
-              <div className="book-img">
-                <img src="https://via.placeholder.com/250" alt="" />
-              </div>
-              <div className="book-info">
-                <div className="book-title">
-                  <h3> Book title </h3>
-                </div>
-                <div className="book-author">
-                  <p> By Author</p>
-                </div>
-                <div className="">
-                  <button className="btn btn-highlight"> Detail</button>
-                </div>
-              </div>
-            </div>
-          </div>
+         <BooksContainer/>
         </div>
       </section>
     </div>
