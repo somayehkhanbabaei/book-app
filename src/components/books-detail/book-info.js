50a42dd9c37592b27760d5e862982b1c28bfa602
somayehkhanbabaei <somayeh.khanbabaei21@gmail.com>
2020-03-15 02:22:17 +0330

    give props

--- src/components/books-detail/book-info.js
+++ src/components/books-detail/book-info.js
@@ -5,18 +5,14 @@ const BookInfo = (props)=> {
     return (
         <div className="book-info">
         <div className="book-title">
-          <h1> Book title </h1>
+          <h1> {props.title} </h1>
         </div>
         <div className="book-author">
-          <h3> book author </h3>
+          <h3> {"By " + props.author} </h3>
         </div>
         <div className="book-description">
           <p>
-            Esse sint consectetur enim sint ullamco. Esse id laborum
-            nostrud anim enim. Tempor deserunt voluptate amet do cupidatat
-            et exercitation ullamco culpa dolor veniam Lorem quis. Commodo
-            excepteur laborum laboris culpa anim do aliquip et laborum
-            nulla aliqua exercitation culpa cupidatat.
+            {props.description}
           </p>
         </div>
       </div>
