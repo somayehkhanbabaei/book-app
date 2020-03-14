9952df0021d808c6904116658a65d27cc0c0c77a
somayehkhanbabaei <somayeh.khanbabaei21@gmail.com>
2020-03-15 02:22:59 +0330

    give data from Api

--- src/components/books-detail/book-container.js
+++ src/components/books-detail/book-container.js
@@ -3,12 +3,24 @@ import BookInfo from "./book-info.js";
 import BookReviewContainer from "./book-review-container.js";
 
 let BookContainer = (props) => {
+  console.log(props)
+  let bookInfo = {
+    image : props.book.items.volumeInfo ? props.book.items.volumeInfo.imageLinks.thumbnail : "https://via.placeholder.com/350x150",
+    title : props.book.items.volumeInfo ? props.book.items.volumeInfo.title : ' No Title',
+    author : props.book.items.volumeInfo ? props.book.items.volumeInfo.authors[0] : "Can't Not Found Authors",
+    description: props.book.items.volumeInfo ? props.book.items.volumeInfo.description: 'No Description',
+    ratings : {
+      averageRating : props.book.items.volumeInfo ? props.book.items.volumeInfo.averageRating:'Nothing',
+      ratingCount : props.book.items.volumeInfo ? props.book.items.volumeInfo.ratingCount: 'No Rating',
+    }
+  }
   return (
     <div className="book-container">
       <div className="book-img">
-        <img src="https://via.placeholder.com/250x200" alt="" />
+        <img src={bookInfo.image} alt={bookInfo.title} />
       </div>
-      <BookInfo />
+      <BookInfo title={bookInfo.title} author={bookInfo.author}
+        description={bookInfo.description}/>
       <BookReviewContainer />
     </div>
   );
