708a3486569b9a9dd283d071096c189cff78c3c2
somayehkhanbabaei <somayeh.khanbabaei21@gmail.com>
2020-03-14 23:43:40 +0330

    render the bookdetailpage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter,Switch,Route} from 'react-router-dom';
 import BooksSectionPage from "./components/pages/books-section.js";
 import BookDetailPage from "./components/pages/book-detail.js";
 import PageNotFound from "./components/pages/page-not-found.js";
+import BookDetail from './components/books-detail/book-detail.js';
 
 class App extends Component {
   render() {
@@ -14,7 +15,10 @@ class App extends Component {
             let {categoryName} = props.match.params;
             return <BooksSectionPage categoryName={categoryName} />
           }}/>  
-          <Route path="/book/:bookID" component={BookDetailPage}/> 
+          <Route path="/book/:bookID" render={(props) => {
+            let {bookID} = props.match.params;
+            return <BookDetailPage bookID={bookID} />
+          }}/> 
           <Route component={PageNotFound}/>
         </Switch>
       </BrowserRouter>
--- src/components/pages/book-detail.js
+++ src/components/pages/book-detail.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "../header/header";
 import Footer from "../footer/footer";
 import BookDetail from "../books-detail/book-detail.js";
 
-const BookDetailPage = props => {
-  return (
-    <div className='has-fixed-footer'>
-      <Header/>
-      <BookDetail/>
-      <Footer/>
-    </div>
-  );
-};
+class BookDetailPage extends Component {
+  constructor(props) {
+    super(props);
+    
+  }
+  render() {
+    return (
+      <div className="has-fixed-footer">
+        <Header />
+        <BookDetail />
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default BookDetailPage;
