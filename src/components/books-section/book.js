4ff1e87ea0a1d7ae16b7404b2b6c13681011e4a1
somayehkhanbabaei <somayeh.khanbabaei21@gmail.com>
2020-03-14 22:39:44 +0330

    get api and assign image , title , author and id

--- src/components/books-section/book.js
+++ src/components/books-section/book.js
@@ -1,25 +1,29 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 
 
 
 const Book = (props) => {
   let bookInfo = {
     image: props.book.volumeInfo.imageLinks.thumbnail,
+    title: props.book.volumeInfo.title,
+    author:props.book.volumeInfo.author,
+    id:props.book.id
   }
   return (
     <div className="book">
     <div className="book-img">
-      <img src="https://via.placeholder.com/250x200" alt="" />
+      <img src= {bookInfo.image} alt={bookInfo.title} />
     </div>
     <div className="book-info">
       <div className="book-title">
-        <h3> Book title </h3>
+        <h3> {bookInfo.title} </h3>
       </div>
       <div className="book-author">
-        <p> By <em> Author </em></p>
+        <p> By <em> {bookInfo.author} </em></p>
       </div>
       <div className="book-call-to-action">
-        <button className="btn btn-highlight"> Detail</button>
+        <a to={"book/" + bookInfo.id} className="btn btn-highlight"> Detail</a>
       </div>
     </div>
   </div>
