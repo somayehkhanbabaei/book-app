23e2dc09461b305b04f703610777950473302022
somayehkhanbabaei <somayeh.khanbabaei21@gmail.com>
2020-03-15 17:25:01 +0330

    create loader page

---
+++ src/components/loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+let Loader = (props) => {
+    return(
+        <div className="loader">
+            <svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg"><circle className="circle" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle></svg>
+
+        </div>
+    )
+}
+
+export default Loader;
